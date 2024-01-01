3b7fd36f2486276188a2bd5c86a0db0769e44776
faizan619 <alamf6023@gmail.com>
2024-01-01 23:16:26 +0530

    footer development start

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import Navbar from './Components/Navbar'
+import Footer from './Components/footer'
 
 export default function Home() {
   return (
     <main className='min-h-screen'>
       <Navbar/>
+      <div>hello</div>
+      <Footer/>
     </main>
   )
 }
+ 
