30f336eca50f011a9d618427d14a23e8ece36c3f
faizan619 <alamf6023@gmail.com>
2024-01-02 17:06:53 +0530

    hero section work started

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import Navbar from './Components/Navbar'
 import Footer from './Components/footer'
+import Hero from './Components/HeroPage'
 
 export default function Home() {
   return (
     <main className='min-h-screen'>
       <Navbar/>
       <div className='min-h-[18.9rem]'>
-        <div id='home' className='border h-screen text-4xl flex justify-center items-center'>Home</div>
+        <div id='home' className='h-screen'><Hero/></div>
         <div id='about' className='border h-screen text-4xl flex justify-center items-center'>About</div>
         <div id='price' className='transition-all border h-screen text-4xl flex justify-center items-center'>Pricing</div>
       </div>
