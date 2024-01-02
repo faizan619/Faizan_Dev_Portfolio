5e88be401210f2937dc21ec64ec9f7ae3eb27312
faizan619 <alamf6023@gmail.com>
2024-01-02 20:52:22 +0530

    home page component added

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,15 @@ import Image from 'next/image'
 import Navbar from './Components/Navbar'
 import Footer from './Components/footer'
 import Hero from './Components/HeroPage'
+import Second from './Components/SecondHero'
 
 export default function Home() {
   return (
     <main className='min-h-screen'>
       <Navbar/>
       <div className='min-h-[18.9rem]'>
-        <div id='home' className='h-screen'><Hero/></div>
-        <div id='about' className='border h-screen text-4xl flex justify-center items-center'>About</div>
+        <div id='home' className='min-h-screen'><Hero/></div>
+        <div id='about' className='min-h-screen'><Second/></div>
         <div id='price' className='transition-all border h-screen text-4xl flex justify-center items-center'>Pricing</div>
       </div>
       <Footer/>
