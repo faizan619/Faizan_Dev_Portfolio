df044e613ea045a3168685bf9abe7b9a49eecbf7
faizan619 <alamf6023@gmail.com>
2024-01-05 00:49:44 +0530

    data of project and skill

---
+++ src/app/api/projectdata.js
@@ -0,0 +1,32 @@
+const Project = [
+    {
+        imgsrc:'https://raw.githubusercontent.com/faizan619/Codo-File/main/documentation/Project%20Assets/JavascriptEditorPage.PNG',
+        name:'CodoFile',
+        projinfo:'It is a online compiler that support javascript,python and dart language.it also support image to text and voice to text feature. Do check the Site to get a details view of  the project.',
+        source:'https://github.com/faizan619/Codo-File',
+        live:'https://codofile-85b0c.web.app/'
+    },
+    {
+        imgsrc:'https://raw.githubusercontent.com/faizan619/Faizan_Dev_Portfolio/main/public/qrcode.png',
+        name:'QR Code Scanner',
+        projinfo:'It is a online compiler that support javascript,python and dart language.it also support image to text and voice to text feature. Do check the Site to get a details view of  the project.',
+        source:'https://github.com/faizan619/Codo-File',
+        live:'https://codofile-85b0c.web.app/'
+    },
+    {
+        imgsrc:'https://raw.githubusercontent.com/faizan619/Faizan_Dev_Portfolio/main/public/blogapp.png',
+        name:'QR Code Scanner',
+        projinfo:'It is a online compiler that support javascript,python and dart language.it also support image to text and voice to text feature. Do check the Site to get a details view of  the project.',
+        source:'https://github.com/faizan619/Codo-File',
+        live:'https://codofile-85b0c.web.app/'
+    },
+    {
+        imgsrc:'https://raw.githubusercontent.com/faizan619/Faizan_Dev_Portfolio/main/public/notepad.png',
+        name:'QR Code Scanner',
+        projinfo:'It is a online compiler that support javascript,python and dart language.it also support image to text and voice to text feature. Do check the Site to get a details view of  the project.',
+        source:'https://github.com/faizan619/Codo-File',
+        live:'https://codofile-85b0c.web.app/'
+    },
+]
+
+export default Project
