424b1745d0f62e572d048c1dd69cd67afa214fbe
faizan619 <alamf6023@gmail.com>
2024-01-04 20:57:04 +0530

    all services page

---
+++ src/app/services/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+// import FormContact from "./FormContact";
+
+export default function Page(){
+    return(
+        <div>
+            <div>hello services</div>
+            {/* <FormContact/> */}
+        </div>
+    )
+}
