0dbfc945da0b2069a5da60b80fa1d6a52e7ef3f4
faizan619 <alamf6023@gmail.com>
2024-01-05 23:42:46 +0530

    light mode color done

--- src/app/Components/Navbar.js
+++ src/app/Components/Navbar.js
@@ -1,19 +1,18 @@
 import "./style/navbar.css";
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between p-5 bg-gray-800 text-white">
-      {/* <div className="font-extrabold text-2xl">FN</div> */}
-      <div className="flex items-center justify-center w-12 h-12 rounded-full dark:bg-gray-600">
+    <nav className="flex items-center justify-between p-5 bg-[#022f69] text-white sticky top-0 z-10">
+      <div className="flex items-center justify-center w-12 h-12 rounded-full border">
               <span className="font-extrabold text-lg">FN</span>
             </div>
       <ul className="nav-links">
         <input type="checkbox" id="checkbox_toggle" />
         <label htmlFor="checkbox_toggle" className="select-none text-2xl cursor-pointer hamburger">&#9776;</label>
-        <div className="flex bg-gray-800 text-center gap-4 text-lg menu">
-          <li className="hover:bg-gray-600"><a href="#home">Home</a></li>
-          <li className="hover:bg-gray-600"><a href="#about">About</a></li>
-          <li className="hover:bg-gray-600 "><a href="#service">Services</a></li>
-          <li className="hover:bg-gray-600"><a href="#project">Projects</a></li>
+        <div className="flex text-center bg-gray-800 gap-4 text-lg menu md:bg-[#022f69] ">
+          <li className="hover:bg-blue-700"><a href="#home">Home</a></li>
+          <li className="hover:bg-blue-700"><a href="#about">About</a></li>
+          <li className="hover:bg-blue-700 "><a href="#service">Services</a></li>
+          <li className="hover:bg-blue-700"><a href="#project">Projects</a></li>
           <li className=" bg-blue-800"><a href="/">Hire me!</a></li>
         </div>
       </ul>
