5b38adf0da5b17ca80059742adcfe0e1ddeaa1a1
faizan619 <alamf6023@gmail.com>
2024-01-05 15:55:42 +0530

    dynamic component created

--- src/app/Components/Service.js
+++ src/app/Components/Service.js
@@ -1,4 +1,5 @@
 "use client"
+import serviceInfo from '../api/servicedata'
 const Service = () => {
   
   return (
@@ -13,14 +14,20 @@ const Service = () => {
         </div>
         <div className="py-5 flex gap-5 flex-wrap justify-center sm:gap-4 md:gap-10 lg:gap-14 xl:gap-16">
 
-          <div className="border flex flex-col items-center px-1 mx-1 py-4 rounded-md gap-2 sm:w-[17rem]">
-              <div  className="flex flex-col items-center">
-                <div className="rounded-full bg-gray-300 text-black p-2 font-semibold">WD</div>
-                <h1 className="text-xl uppercase font-semibold">Website Creation</h1>
-              </div>
-              <p className="text-gray-500">We <span className="text-white capitalize">specialize</span> in delivering superior <span className="text-white capitalize">website designs</span> featuring a <span className="text-white capitalize">user-friendly UI</span>. Consider giving us an opportunity, and we will undoubtedly prove that choosing us is the optimal decision for your needs.</p>
-              <button className="border py-2 rounded-md bg-gray-600 px-10">Contact</button>
-          </div>
+        {
+          serviceInfo.map((item)=>(
+            <div className="border flex flex-col justify-between items-center px-1 sm:px-3 mx-1 py-4 rounded-md gap-2 sm:w-[20rem] h-[20rem]">
+                <div  className="flex flex-col items-center gap-3">
+                  <div className="rounded-full bg-gray-300 text-black p-2 font-semibold">{item.logo}</div>
+                  <h1 className="text-xl uppercase font-semibold">{item.name}</h1>
+                </div>
+                {/* {item.serviceinfo} */}
+                <p className="text-gray-500">{item.serviceinfo}</p>
+                <a href='mailto:alamf6023@gmail.com'><button className="border py-2 rounded-md bg-gray-600 px-10">Mail Us</button></a>
+            </div>
+          ))
+        }
+
           
         
         </div>
