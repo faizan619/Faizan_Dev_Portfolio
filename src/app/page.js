19a512d1852335e52b0b6be58f44b48007033b20
faizan619 <alamf6023@gmail.com>
2024-01-05 23:42:20 +0530

    minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -8,20 +8,20 @@ import Service from './Components/Service'
 import FormContact from './Components/FormContact'
 import Project from './Components/Project'
 
-export default function Home() {
+export default function Home() { 
   
   return (
     <main className='min-h-screen'>
       <Navbar/>
       {/* <div className='min-h-[18.9rem]'> */}
       <div className=''>
-        <div id='home' className=''><Hero/></div>
+        <div id='home' className=''><Hero/></div> 
         <div id='about' className='min-h-screen'><Second/></div>
-        <div><Skill/></div>
+        <div><Skill/></div>  
         <div id='service' className='min-h-screen '><Service/></div>
         <div id='project' className='min-h-screen'><Project/></div>
         <div id='' className=''><FormContact/></div>
-      </div>
+      </div> 
       <Footer/>
     </main>
   )
