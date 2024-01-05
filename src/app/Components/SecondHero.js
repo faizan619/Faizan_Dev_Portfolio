0344072f9fa1ced5ffcc7249037b627ca6e09826
faizan619 <alamf6023@gmail.com>
2024-01-05 23:53:07 +0530

    light mode design done

--- src/app/Components/SecondHero.js
+++ src/app/Components/SecondHero.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import MyImg from "../../../public/faizan1.png";
 export default function Second() {
   return (
-    <section className="">
+    <section className="bg-black text-white">
       <div className="flex flex-col items-center">
         <h1 className="text-3xl sm:text-4xl font-semibold mt-4 text-center">About Me</h1>
         <div className="px-3 flex flex-col items-center md:flex-row  ">
@@ -42,13 +42,13 @@ export default function Second() {
                     <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                   </svg>
                 </span>
-                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-blue-800 dark:text-white">
                   MERN Stack Developer Course
-                  <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">
+                  <span className="bg-blue-800 text-blue-100 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">
                     Latest
                   </span>
                 </h3>
-                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                <time className="block mb-2 text-sm font-normal leading-none text-white dark:text-gray-500">
                   Completed on December 28th, 2023
                 </time>
                 <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
@@ -76,10 +76,10 @@ export default function Second() {
                     <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                   </svg>
                 </span>
-                <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                <h3 className="mb-1 text-lg font-semibold text-blue-800 dark:text-white">
                   Flowbite Figma v1.3.0
                 </h3>
-                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                <time className="block mb-2 text-sm font-normal leading-none text-white dark:text-gray-500">
                   Released on December 7th, 2021
                 </time>
                 <p className="text-base font-normal text-gray-500 dark:text-gray-400">
@@ -100,10 +100,10 @@ export default function Second() {
                     <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                   </svg>
                 </span>
-                <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                <h3 className="mb-1 text-lg font-semibold text-blue-800 dark:text-white">
                   Flowbite Library v1.2.2
                 </h3>
-                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                <time className="block mb-2 text-sm font-normal leading-none text-white dark:text-gray-500">
                   Released on December 2nd, 2021
                 </time>
                 <p className="text-base font-normal text-gray-500 dark:text-gray-400">
@@ -123,13 +123,13 @@ export default function Second() {
                     <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                   </svg>
                 </span>
-                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-blue-800 dark:text-white">
                   Flowbite Application UI v2.0.0{" "}
-                  <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">
+                  <span className="bg-blue-800 text-blue-100 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">
                     Latest
                   </span>
                 </h3>
-                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                <time className="block mb-2 text-sm font-normal leading-none text-white dark:text-gray-500">
                   Released on January 13th, 2022
                 </time>
                 <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
@@ -166,10 +166,10 @@ export default function Second() {
                     <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                   </svg>
                 </span>
-                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-                  Flowbite Application UI v2.0.0{" "}
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-blue-800 dark:text-white">
+                  Flowbite Application UI v2.0.0
                 </h3>
-                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                <time className="block mb-2 text-sm font-normal leading-none text-white dark:text-gray-500">
                   Released on January 13th, 2022
                 </time>
                 <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
@@ -207,10 +207,10 @@ export default function Second() {
                     <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                   </svg>
                 </span>
-                <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                <h3 className="mb-1 text-lg font-semibold text-blue-800 dark:text-white">
                   Flowbite Figma v1.3.0
                 </h3>
-                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                <time className="block mb-2 text-sm font-normal leading-none text-white dark:text-gray-500">
                   Released on December 7th, 2021
                 </time>
                 <p className="text-base font-normal text-gray-500 dark:text-gray-400">
