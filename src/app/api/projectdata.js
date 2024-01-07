8dbb0c13317923412f585642e0d1e9b78f418619
faizan619 <alamf6023@gmail.com>
2024-01-07 20:51:43 +0530

    project data fixed

--- src/app/api/projectdata.js
+++ src/app/api/projectdata.js
@@ -3,7 +3,7 @@ const Project = [
         id:1,
         imgsrc:'https://raw.githubusercontent.com/faizan619/Codo-File/main/documentation/Project%20Assets/JavascriptEditorPage.PNG',
         name:'CodoFile',
-        projinfo:'It is a online compiler that support javascript,python and dart language.it also support image to text and voice to text feature. Do check the Site to get a details view of  the project.',
+        projinfo:'It is an online compiler that supports JavaScript, Python, and Dart languages. It also supports image-to-text and voice-to-text features. Do check the site to get a detailed view of the project.',
         source:'https://github.com/faizan619/Codo-File',
         live:'https://codofile-85b0c.web.app/'
     },
@@ -11,25 +11,25 @@ const Project = [
         id:2,
         imgsrc:'https://raw.githubusercontent.com/faizan619/Faizan_Dev_Portfolio/main/public/qrcode.png',
         name:'QR Code Scanner',
-        projinfo:'It is a online compiler that support javascript,python and dart language.it also support image to text and voice to text feature. Do check the Site to get a details view of  the project.',
-        source:'https://github.com/faizan619/Codo-File',
-        live:'https://codofile-85b0c.web.app/'
+        projinfo:'It is a QR code scanner and reader. If you have a QR code, you can use the reader feature to decode the QR code or create your own QR code just by inputting the text.',
+        source:'https://github.com/faizan619/QRCodeGenerator',
+        live:'https://wondrous-hamster-93ebe9.netlify.app/'
     },
     {
         id:3,
         imgsrc:'https://raw.githubusercontent.com/faizan619/Faizan_Dev_Portfolio/main/public/blogapp.png',
-        name:'QR Code Scanner',
-        projinfo:'It is a online compiler that support javascript,python and dart language.it also support image to text and voice to text feature. Do check the Site to get a details view of  the project.',
-        source:'https://github.com/faizan619/Codo-File',
-        live:'https://codofile-85b0c.web.app/'
+        name:'Public Blog Site',
+        projinfo:'A simple blog app created using React and Firebase. It is just a basic demonstration of the blog. You can write or edit blog posts on this site. The credentials for deleting blog posts are accessible only to the admin.',
+        source:'https://github.com/faizan619/Public-BlogPage',
+        live:'https://blog.faizanalam.tech'
     },
     {
         id:4,
-        imgsrc:'https://raw.githubusercontent.com/faizan619/Faizan_Dev_Portfolio/main/public/notepad.png',
-        name:'QR Code Scanner',
-        projinfo:'It is a online compiler that support javascript,python and dart language.it also support image to text and voice to text feature. Do check the Site to get a details view of  the project.',
-        source:'https://github.com/faizan619/Codo-File',
-        live:'https://codofile-85b0c.web.app/'
+        imgsrc:'https://raw.githubusercontent.com/faizan619/Faizan_Dev_Portfolio/main/public/chatapp.png',
+        name:'Chatting App',
+        projinfo:'It is a chatting app created using vanilla JavaScript and Socket.js. This chatting app supports real-time messaging with users by creating a separate room for every individual or group.',
+        source:'https://github.com/faizan619/DaBoys-ChatApp',
+        live:'https://da-boys.onrender.com'
     },
 ]
 
