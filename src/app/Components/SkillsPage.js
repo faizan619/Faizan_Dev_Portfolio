4273783bd48291a1ae3fb52c5e9566b3d1eec011
faizan619 <alamf6023@gmail.com>
2024-01-07 22:00:06 +0530

    design done

--- src/app/Components/SkillsPage.js
+++ src/app/Components/SkillsPage.js
@@ -2,13 +2,13 @@ import Data from "../api/skilldata"
 export default function Skill(){
 
     
-    return(
+    return( 
         <div className="p-3 py-10 text-center min-h-[15rem] bg-[#01003d] text-white">
             <h1 className="text-3xl p-5 font-extrabold sm:text-4xl">Skills</h1>
             <div className="flex flex-wrap gap-4 justify-center">
             {
                 Data.map((item)=>(
-                    <div className="bg-gray-800 capitalize px-4 py-1 rounded-md text-gray-200">{item.lang}</div>
+                    <div key={item.id} className="bg-blue-800 capitalize px-4 py-1 rounded-md text-gray-200">{item.lang}</div>
                 ))
             }
 
