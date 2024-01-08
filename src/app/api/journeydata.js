32ae9cead3a88435596f11277215908a7b772766
faizan619 <alamf6023@gmail.com>
2024-01-08 14:57:12 +0530

    more update needed

---
+++ src/app/api/journeydata.js
@@ -0,0 +1,61 @@
+// export const Latest = [
+//     {
+//         id:1,
+//         latest:true,
+//         title:'i am title',
+//         date:'18/6/2002',
+//         info:'i am the info',
+//         certificate:'google.com'
+//     }
+// ]
+
+const JourneyData = [
+    {
+        id:1,
+        latest:true,
+        title:'MERN Stack Developer Course',
+        date:'28 December, 2023',
+        info:'In this course, I have gained knowledge about advanced topics in React. Additionally, I have acquired insights into backend development using Node.js for the creation of a comprehensive blog web application.',
+        certificate:"https://raw.githubusercontent.com/faizan619/Faizan_Dev_Portfolio/main/public/faizan_mern_certificate.jpg"
+    },{
+        id:2,
+        latest:false,
+        title:"Node JS Certification Course",
+        date:'24 September 2023',
+        info:'In this course,I gained the knowledge of Javascript framework(Node.js) for getting the depth knowledge of backend technology',
+        certificate:'https://moonshot.scaler.com/s/li/PffYF6bYvl'
+    },{
+        id:3,
+        latest:false,
+        title:'Internship in Website Development',
+        date:'12 Augest 2023',
+        info:'Completed the Website Development internship from CodSoft.',
+        certificate:'https://media.licdn.com/dms/image/D4D22AQHu6gnLvfSFhQ/feedshare-shrink_800/0/1692435083113?e=1707350400&v=beta&t=PRtRJ93juxSrrMjwW9z7lFJ-BGdoJgfPsEwTimXgnsA'
+    },{
+        id:3,
+        latest:false,
+        title:'Google UX Design',
+        date:'10 September 2022', 
+        info:'Completed UX design course held by Google in coursera.I help me to understand the Wireframming and Prototyping.Also it thought me about the user view about a particular screen of a software.',
+        certificate:'https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~64DQPXYHFP3D/CERTIFICATE_LANDING_PAGE~64DQPXYHFP3D.jpeg'
+    },{
+        id:4,
+        latest:false,
+        title:"1st Position in Webite Competition",
+        date:'11 Augest, 2023',
+        info:'A Website Development Competition held in LN College, Borivali(E)',
+        certificate:'https://raw.githubusercontent.com/faizan619/Faizan_Dev_Portfolio/main/public/webdev1.jpg'
+    },{
+        id:4,
+        latest:false,
+        title:"1st in Poster Competition",
+    },{
+        id:5,
+        latest:false,
+        title:"FY Completed of Computer Science",
+        date:'9th May ,2022',
+        info:'Completed my First year of Computer Science with 9.2 CGPA.'
+    }
+]
+
+export default JourneyData
