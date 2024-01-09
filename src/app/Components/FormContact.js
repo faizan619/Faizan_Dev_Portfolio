a238d73e5b1e5350a7373cb4d26b40c75b359c2a
faizan619 <alamf6023@gmail.com>
2024-01-09 22:35:29 +0530

    dark mode design

--- src/app/Components/FormContact.js
+++ src/app/Components/FormContact.js
@@ -24,11 +24,11 @@ export default function FormContact() {
   };
 
   return (
-    <section className="py-6 bg-white text-[#01003d]">
+    <section className="dark:bg-[#1c1e29] dark:text-[#4171f5] py-6 bg-slate-100 text-[#01003d]">
       <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x">
         <div className="py-6 md:py-0 md:px-6 flex flex-col justify-center">
           <h1 className="text-3xl sm:text-5xl font-bold">Get in touch</h1>
-          <p className="pt-2 pb-4 text-gray-700 text-lg">
+          <p className="pt-2 pb-4 text-gray-700 text-lg dark:text-gray-300">
             Fill in the form to start a conversation
           </p>
           <div className="space-y-6 py-5">
@@ -45,7 +45,7 @@ export default function FormContact() {
                   clipRule="evenodd"
                 ></path>
               </svg>
-              <span className="text-xl">Mumbai, Maharashtra, India</span>
+              <span className="text-xl dark:text-white">Mumbai, Maharashtra, India</span>
             </p>
             <p className="flex items-center">
               <svg
@@ -56,7 +56,7 @@ export default function FormContact() {
               >
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
               </svg>
-              <span className="text-xl">+91 9987337815</span>
+              <span className="text-xl dark:text-white">+91 9987337815</span>
             </p>
             <p className="flex items-center">
               <svg
@@ -68,13 +68,13 @@ export default function FormContact() {
                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
               </svg>
-              <span className="text-xl">alamf6023@gmail.com</span>
+              <span className="text-xl dark:text-white ">alamf6023@gmail.com</span>
             </p>
           </div>
         </div>
         <form
         //   noValidate=""
-          className="flex flex-col space-y-7 md:py-5 md:px-6 text-black border-[#01003d]"
+          className="flex flex-col space-y-7 md:py-5 md:px-6 text-black dark:text-white border-[#01003d]"
         >
           <label className="block">
             <span className="mb-1 ">Full name</span>
@@ -108,7 +108,7 @@ export default function FormContact() {
           <button
             type="button"
             onClick={addFeedback}
-            className="self-center px-8 py-3  text-lg rounded focus:ring hover:ring border border-[#01003d] text-white bg-[#01003d]"
+            className="dark:bg-[#4171f5] dark:border-white self-center px-8 py-3  text-lg rounded focus:ring hover:ring border border-[#01003d] text-white bg-[#01003d]"
           >
             Submit
           </button>
